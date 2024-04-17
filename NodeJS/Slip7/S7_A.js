cf774097f336a4b8700ebae88eb6cc97479a2d38
Omkar Mandhare <omkarmandhare530@gmail.com>
2024-04-17 22:09:12 +0530

    Minor Changes

--- NodeJS/Slip7/S7_A.js
+++ NodeJS/Slip7/S7_A.js
@@ -1,6 +1,6 @@
-import http from "http";
-import url from "url";
-import fs from "fs";
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
 const PORT = 8000;
 
 const fileName = "./index.html";
